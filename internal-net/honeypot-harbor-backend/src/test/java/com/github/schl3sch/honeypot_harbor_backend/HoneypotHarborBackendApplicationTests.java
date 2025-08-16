55e901b764a099e1078d7e28306aaa0cbd91e858
schl3sch <schlegelj.tin23@student.dhbw-heidenheim.de>
2025-08-16 14:00:37 +0200

    initialize springboot backend

---
+++ internal-net/honeypot-harbor-backend/src/test/java/com/github/schl3sch/honeypot_harbor_backend/HoneypotHarborBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.schl3sch.honeypot_harbor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneypotHarborBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
