d137feb1f2fc1be07b9c904ee338f1a296f0fb43
simoneggr <eggers.tin23@student.dhbw-heidenheim.de>
2025-09-14 16:00:34 +0000

    Change cowrie connect script and remove chmod from startup

--- connect_cowrie.sh
+++ connect_cowrie.sh
@@ -5,6 +5,8 @@ containers=("192.168.1.131" "192.168.1.132" "192.168.1.133")
 port=2222
 
 for ip in "${containers[@]}"; do
-	echo "Verbinde zu $ip ..."
-	ssh -p "$port" "$ip"
+	for i in {1..10}; do
+		echo "[$i/10] Connect to $ip ..."
+		ssh -p "$port" "$ip"
+	done
 done
