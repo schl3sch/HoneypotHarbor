77380f07acf239150e0af5eb5263dd8fc83534f7
schl3sch <schlegelj.tin23@student.dhbw-heidenheim.de>
2025-09-16 10:37:49 +0200

    layout for Documentation

--- wifi-shutdown.sh
+++ wifi-shutdown.sh
@@ -6,31 +6,30 @@
 
 set -e
 
-echo "Stopping Gateway:"
+echo "Stopping Gateway."
 docker compose -f gateway/docker-compose.yml down
 
-echo "Stopping Attacker Network:"
+echo "Stopping Attacker Network:.
 docker compose -f attacker-net/docker-compose.yml --env-file attacker-net/attacker.env down
 
-echo "Stopping Internal Network:"
+echo "Stopping Internal Network."
 docker compose -f internal-net/docker-compose.yml --env-file internal-net/internal.env down
 
-echo "Removing Networks:"
+echo "Removing Networks."
 docker network rm honeypotharbor-internal-network honeypotharbor-attacker-network
 
-echo "Removing host macvlan interfaces and routes..."
-# Internal network
+echo "Removing host macvlan interfaces and routes."
 sudo ip route del 192.168.1.0/25 dev internal-net
 sudo ip link set internal-net down
 sudo ip link delete internal-net
 
-# Attacker network
 sudo ip route del 192.168.1.128/25 dev attacker-net
 sudo ip link set attacker-net down
 sudo ip link delete attacker-net
 
-echo "Removing veth interfaces"
+echo "Removing veth interfaces."
 if ip link show veth0 &>/dev/null; then
+  sudo ip link set veth0 down
   sudo ip link delete veth0
 fi
 
--- wifi-startup.sh
+++ wifi-startup.sh
@@ -7,13 +7,13 @@
 set -e
 
 if ! ip link show veth0 &>/dev/null; then
-  echo "Creating veth interface pair"
+  echo "Creating veth interface pair."
   sudo ip link add veth0 type veth peer name veth1
   sudo ip link set veth0 up
   sudo ip link set veth1 up
 fi
 
-echo "Create Networks:"
+echo "Create Networks."
 docker network create -d macvlan \
   --subnet=192.168.1.0/25 \
   --gateway=192.168.1.1 \
@@ -28,21 +28,21 @@ docker network create -d macvlan \
   -o parent=veth0 \
   honeypotharbor-attacker-network
 
-echo "Create internal Macvlan shim for Host-access:"
+echo "Create internal Macvlan shim for Host-access."
 sudo ip link add internal-net link veth0 type macvlan mode bridge
 sudo ip addr add 192.168.1.126/25 dev internal-net 
 sudo ip link set internal-net up
 sudo ip route del 192.168.1.0/25 dev internal-net 2>/dev/null
 sudo ip route add 192.168.1.0/25 dev internal-net
 
-echo "Create attacker Macvlan shim for Host-access:"
+echo "Create attacker Macvlan shim for Host-access."
 sudo ip link add attacker-net link veth0 type macvlan mode bridge
 sudo ip addr add 192.168.1.254/25 dev attacker-net
 sudo ip link set attacker-net up
 sudo ip route del 192.168.1.128/25 dev attacker-net 2>/dev/null
 sudo ip route add 192.168.1.128/25 dev attacker-net
 
-echo "Prepare Cowrie directories and permissions..."
+echo "Prepare Cowrie directories and permissions."
 for i in 1 2 3; do
     sudo mkdir -p attacker-net/logs/cowrie$i
     sudo touch attacker-net/logs/cowrie$i/cowrie.json
@@ -50,11 +50,13 @@ for i in 1 2 3; do
     sudo chmod -R 775 attacker-net/logs/ attacker-net/cowrie-etc/
 done
 
-echo "Startup Internal Network:"
+echo "Startup Internal Network."
 docker compose -f internal-net/docker-compose.yml --env-file internal-net/internal.env up -d
 
-echo "Startup Attacker Network:"
+echo "Startup Attacker Network."
 docker compose -f attacker-net/docker-compose.yml --env-file attacker-net/attacker.env up -d
 
-echo "Startup Gateway:"
+echo "Startup Gateway."
 docker compose -f gateway/docker-compose.yml up -d
+
+echo "All networks and interfaces are setup."
